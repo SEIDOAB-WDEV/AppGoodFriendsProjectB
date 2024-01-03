bb9728ef9c5be120031e78f39b662143eea5919f
Martin <martin_lenart@icloud.com>
2024-01-03 09:48:20 +0100

    di done

--- AppRazor/Program.cs
+++ AppRazor/Program.cs
@@ -1,7 +1,26 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using DbRepos;
+using Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+#region Dependency Inject FriendsService
+//Services are typically added as Scoped as one scope is a Web client request
+//- Transient objects are always different in the IndexModel and in the middleware.
+//- Scoped objects are the same for a given request but differ across each new request.
+//- Singleton objects are the same for every request.
+
+//DI injects the DbRepos into csFriendService
+//Services are typically added as Scoped as one scope is a Web client request
+builder.Services.AddScoped<csFriendsDbRepos>();
+
+//WebController have a matching constructor, so service must be created
+//Services are typically added as Scoped as one scope is a Web client request
+//builder.Services.AddSingleton<IFriendsService, csFriendsServiceModel>();
+builder.Services.AddScoped<IFriendsService, csFriendsServiceDb>();
+#endregion
 
 var app = builder.Build();
 
