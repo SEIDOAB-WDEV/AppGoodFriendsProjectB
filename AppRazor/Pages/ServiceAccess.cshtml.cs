2303cdd023a1795c6db46b425f560da4857fffb3
Martin <martin_lenart@icloud.com>
2024-01-11 11:17:48 +0100

    update

---
+++ AppRazor/Pages/ServiceAccess.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppRazor.Pages
+{
+    public class ServiceAccessModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostNewFriend()
+        {
+            return Page();
+        }
+    }
+}
